a2bc72aee6ca86901ef02f222aaa6c4a671de36e
Aaron Conklin <conklin.aaron@gmail.com>
2017-04-05 19:02:42 -0400

    thorough media queries

--- scripts.js
+++ scripts.js
@@ -264,7 +264,7 @@ $(function() {
       link: 'https://amcon.github.io/N_Interactive_quiz',
       name: quiz_title,
       description: quiz_description,
-      image: 'http://i.imgur.com/ZVUOUJ6.jpg'
+      picture: quiz_pic
     }, function(response){});
   });
 
